2320ffda668274d887760532d9b5811d0d5ee482
ali-ajelandrox <109735754+ali-ajelandrox@users.noreply.github.com>
2023-12-21 00:58:09 -0300

    se suman desafio 1 y 2

---
+++ 1_funcion.js
@@ -0,0 +1,12 @@
+
+//Transforma esta declaración de función a una función de expresión. El ejercicio debe
+//quedar en un archivo nombrado
+
+function example (a,b,c){
+    return a+b+c
+}
+
+//respuesta
+let example = function (a,b,c){
+    return a+b+c
+}
---
+++ 2_arrow.js
@@ -0,0 +1,12 @@
+//Transforma la siguiente función a una arrow function de una línea. Este ejercicio
+//debe quedar en un archivo llamado 
+
+
+//let suma = function(a, b){
+//    return a + b
+//   }
+
+//respuesta
+
+let suma = (a,b) =>  a + b;
+
